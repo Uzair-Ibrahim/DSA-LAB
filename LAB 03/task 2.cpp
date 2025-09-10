7a68e34b84f8af523ae9340abd08b825cfb59477
UZAIR IBRAHIM <uzairkamboh311@gmail.com>
2025-09-10 14:30:26 +0500

    Update task 2.cpp

--- LAB 03/task 2.cpp
+++ LAB 03/task 2.cpp
@@ -1,55 +1,71 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
 class Node{
-public:
+    public:
     int data;
     Node* next;
-    Node(int val){data=val;next=NULL;}
+    Node(int val){
+        data=val;
+        next=NULL;
+    }
 };
-class LinkedList{
-private:
+
+class List{
     Node* head;
     Node* tail;
-public:
-    LinkedList(){head=NULL;tail=NULL;}
-    void insertEnd(int val){
+    public:
+    List(){
+        head=tail=NULL;
+    }
+
+    void displayll(){
+        Node* temp=head;
+        while(temp!=NULL){
+            cout<<temp->data<<" -> ";
+            temp=temp->next;
+        }
+        cout<<"NULL"<<endl;
+    }
+
+    void pushback(int val){
         Node* newNode=new Node(val);
-        if(head==NULL){head=tail=newNode;return;}
+        if(head==NULL){
+            head=tail=newNode;
+            return;
+        }
+        
         tail->next=newNode;
-        tail=newNode;
+        tail=newNode;   
     }
-    void rotate(int k){
-        if(head==NULL||k==0) return;
-        Node* temp=head;
-        int count=1;
-        while(count<k && temp!=NULL){temp=temp->next;count++;}
-        if(temp==NULL) return;
-        Node* newHead=temp->next;
-        if(newHead==NULL) return;
+
+    void rotate(int num){
+        Node*temp=head;
+        Node*newhead=NULL;
+        for(int i=0; i<num-1; i++){
+            temp=temp->next;
+        }
+        newhead=temp->next;
         tail->next=head;
-        head=newHead;
-        tail=temp;
-        tail->next=NULL;
-    }
-    void printList(){
-        Node* temp=head;
-        while(temp!=NULL){cout<<temp->data<<" ";temp=temp->next;}
-        cout<<endl;
+        tail=head;
+        head=newhead;
+        temp->next=NULL;
     }
 };
-int main(){
-    LinkedList list;
-    int k;
-    int arr[]={5,3,1,8,6,4,2};
-    for(int i=0;i<7;i++) {
-        list.insertEnd(arr[i]);
-    }
-    cout<<"Given list: ";
-    list.printList();
-    cout<<"Enter the number: ";
-    cin>>k;
-    list.rotate(k);
-    cout<<"After rotation: ";
-    list.printList();
+
+int main() {
+    
+    List ll;
+
+    ll.pushback(1);
+    ll.pushback(2);
+    ll.pushback(3);
+    ll.pushback(4);
+
+    ll.rotate(2);
+
+    ll.displayll();
+
     return 0;
+    
 }
