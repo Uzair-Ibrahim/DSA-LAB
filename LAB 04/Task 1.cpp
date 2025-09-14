230494f487510753d125b68fe6427bae671c3985
UZAIR IBRAHIM <uzairkamboh311@gmail.com>
2025-09-14 22:35:44 +0500

    Update Task 1.cpp

--- LAB 04/Task 1.cpp
+++ LAB 04/Task 1.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int a[10];
-    for(int i=0;i<10;i++) cin>>a[i];
+    int arr[10];
+    for(int i=0;i<10;i++) cin>>arr[i];
     for(int i=0;i<10;i++){
         int m=i;
         for(int j=i+1;j<10;j++){
-            if(a[j]<a[m]) m=j;
+            if(arr[j]<arr[m]) m=j;
         }
-        swap(a[i],a[m]);
+        swap(arr[i],arr[m]);
     }
-    for(int i=0;i<4;i++) cout<<a[i]<<" ";
+    for(int i=0; i<4; i++) cout<<arr[i]<<" ";
 }
