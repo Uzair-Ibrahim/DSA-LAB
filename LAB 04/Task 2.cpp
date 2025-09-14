9a3a62663500fe47b5f3fb9e8f9d0d07803acc4c
UZAIR IBRAHIM <uzairkamboh311@gmail.com>
2025-09-14 22:37:50 +0500

    Update Task 2.cpp

--- LAB 04/Task 2.cpp
+++ LAB 04/Task 2.cpp
@@ -1,20 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int a[9]={20,12,15,2,10,1,13,9,5},n=9;
+    int arr[9]={20,12,15,2,10,1,13,9,5},n=9;
     for(int i=1;i<n;i++){
-        int k=a[i],j=i-1;
-        while(j>=0&&a[j]>k){
-          a[j+1]=a[j];j--;
+        int k=arr[i],j=i-1;
+        while(j>=0&&arr[j]>k){
+          arr[j+1]=arr[j];
+            j--;
         }
-        a[j+1]=k;
+        arr[j+1]=k;
     }
-    int b[9],m=n/2; 
-    b[m]=a[n-1]; 
+    int brr[9],m=n/2; 
+    brr[m]=arr[n-1]; 
     int x=0;
     for(int i=0;i<n;i++){
       if(i==m) continue; 
-      b[i]=a[x++];
+      brr[i]=arr[x++];
     }
-    for(int i=0;i<n;i++) cout<<b[i]<<" ";
+    for(int i=0;i<n;i++) cout<<brr[i]<<" ";
 }
