70c69bbc809488c1aa7f131483ff6dec51740cb2
UZAIR IBRAHIM <uzairkamboh311@gmail.com>
2025-09-27 22:27:59 +0500

    Update lab task 2.cpp

--- LAB 05/lab task 2.cpp
+++ LAB 05/lab task 2.cpp
@@ -1,1 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int secret;
+
+void play(int p) {
+    int g;
+    cout << "player " << p << " guess: ";
+    cin >> g;
+    if (g == secret) {
+        cout << "player " << p << " wins! num was " << secret << endl;
+        return;
+    }
+    if (g < secret) cout << "too low" << endl;
+    else cout << "too high" << endl;
+    play(p == 1 ? 2 : 1);
+}
+
+int main() {
+    srand(time(0));
+    secret = rand() % 100 + 1;
+    cout << "game start"<< endl;
+    play(1);
+}
