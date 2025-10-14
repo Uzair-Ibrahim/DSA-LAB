e13ce874b4aa915124d1a151330ab62f69aa6b4b
UZAIR IBRAHIM <uzairkamboh311@gmail.com>
2025-10-13 23:25:11 -0700

    Create Task 4.cpp

---
+++ LAB 06/Task 4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+bool isSafeQueen(int board[][20],int row,int col,int N){
+    for(int i=0;i<col;i++) if(board[row][i]) return false;
+    for(int i=row,j=col;i>=0 && j>=0;i--,j--) if(board[i][j]) return false;
+    for(int i=row,j=col;i<N && j>=0;i++,j--) if(board[i][j]) return false;
+    return true;
+}
+bool solveNQUtil(int board[][20],int col,int N){
+    if(col>=N) return true;
+    for(int i=0;i<N;i++){
+        if(isSafeQueen(board,i,col,N)){
+            board[i][col]=1;
+            if(solveNQUtil(board,col+1,N)) return true;
+            board[i][col]=0;
+        }
+    }
+    return false;
+}
+int main(){
+    int N=4;
+    int board[20][20]={0};
+    if(solveNQUtil(board,0,N)){
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++) cout<<(board[i][j]?"Q ":"- ");
+            cout<<endl;
+        }
+    }else cout<<"No solution"<<endl;
+    return 0;
+}
