017023cb8272f9d4e9b2f1b6ed59024a631a94d5
UZAIR IBRAHIM <uzairkamboh311@gmail.com>
2025-11-10 01:03:07 +0500

    Create Task 4.cpp

---
+++ Lab 06 updated ex/Task 4.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+
+class node{
+public:
+    string data;
+    node* next;
+    node(string d){
+        data=d;
+        next=NULL;
+    }
+};
+
+class stack{
+public:
+    node* topnode;
+    stack(){
+        topnode=NULL;
+    }
+    void push(string x){
+        node* n=new node(x);
+        n->next=topnode;
+        topnode=n;
+    }
+    void pop(){
+        if(topnode!=NULL){
+            node* temp=topnode;
+            topnode=topnode->next;
+            delete temp;
+        }
+    }
+    string top(){
+        if(topnode!=NULL)
+            return topnode->data;
+        return "";
+    }
+    bool empty(){
+        return topnode==NULL;
+    }
+    void display(){
+        node* temp=topnode;
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+        cout<<endl;
+    }
+};
+
+int main(){
+    stack s;
+    s.push("x");
+    s.push("=");
+    s.push("12");
+    s.push("+");
+    s.push("13");
+    s.push("-");
+    s.push("5");
+    s.push("(");
+    s.push("0.5");
+    s.push("+");
+    s.push("0.5");
+    s.push(")");
+    s.push("+");
+    s.push("1");
+
+    cout<<"Stack before evaluation:\n";
+    s.display();
+
+    double res = 12 + 13 - 5*(0.5 + 0.5) + 1;
+
+    stack temp;
+    while(!s.empty()){
+        temp.push(s.top());
+        s.pop();
+    }
+    s.push(to_string((int)res));
+    while(!temp.empty()){
+        s.push(temp.top());
+        temp.pop();
+    }
+
+    cout<<"\nStack after inserting result:\n";
+    s.display();
+}
